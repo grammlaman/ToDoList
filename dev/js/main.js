e0451e941e7f29ed0d8223f04e60017411fa4988
Серикбаев Дамир <sdmmds93@gmail.com>
2020-01-20 21:01:08 +0600

    На занятии 20 января

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,4 +1,10 @@
-
+function Counter(x = 0){
+    i = x;
+    return function(){
+        return i = i + 1;
+    }
+}
+let idGenerate = Counter();
 class Task{
     constructor(taskData){
         const _ = this;
@@ -10,40 +16,99 @@ class Task{
         _.description = taskData['description'] || '';
         _.catID = taskData['catID'] || 0;	//ID Категории
         _.date = taskData['date'];
-        let data = new Date;
+        _.marks = taskData['marks'] || [];	//Массив идентификаторов меток
         if(!taskData['date']){
             _.date = data.getYear + '-' + data.getMonth + '-' + data.getDate;
         }
-        _.marks = taskData['marks'] || [];	//Массив идентификаторов меток
+        _.bufferId();
+    }
+    bufferId(){
+        const _ = this;
+        localStorage.setItem('bufferId', _.id);
     }
 }
 
-function Counter(x = 0){
-    i = x;
-    return function(){
-        return i = i + 1;
-    }
-}
-let idGenerate = Counter();
+let months = {
+    'ru': [
+        'Январь',
+        'Февраль',
+        'Март',
+        'Апрель',
+        'Май',
+        'Июнь',
+        'Июль',
+        'Август',
+        'Сентябрь',
+        'Октябрь',
+        'Ноябрь',
+        'Декабрь'
+    ]
+};
 
 class List{
     constructor(){
         const _ = this;
         //Свойства
-        _.currentMonth = 0;
-        _.currentDay = 0;
-
+        /*_.currentMonth = 0;
+        _.currentDay = 0;*/
+        _.currentLang = 'ru';
         _.tasks = {};
-        _.currentTask = {};
+        _.lastTask = {};
+        //_.getTask();
+        _.currentFullDate = _.getCurrentDate();
+        _.init();
     }
     //Методы выборки
-    getCategory(){}
-    getMark(){}
-    getCurrent(){}
-    getDay(){}
-    getMonth(){}
-    getYear(){}
-    getAll(){}
+    getMonthDays(month){}
+    getLabels(day){}
+    getCategories(){}
+    getTasks(month){
+        const _ = this;
+        if(localStorage.getItem('tasks')){
+            let tasks = JSON.parse(localStorage.getItem('tasks'));
+            for(let prop in tasks){
+                let currentTasks = tasks[prop];
+                for(let i=0;i < currentTasks.length;i++){
+                    _.addTask(currentTasks[i]);
+                }
+            }
+            //_.tasks = JSON.parse(localStorage.getItem('tasks'));
+        }
+    }
+    getCurrentDate(dateStr=''){
+        const _ = this;
+        let date = new Date();
+        if(dateStr){
+            date = new Date(dateStr);
+        }
+        let
+            month = date.getMonth(),
+            year = date.getFullYear(),
+            monthDay = date.getDate(),
+            weekDay = date.getDay();
+
+        if( (month == 3) || (month == 5) || (month == 8) || (month == 10) )
+            _.daysInMonth = 30;
+        else if(month == 1)
+            if((year%4) == 0){
+                _.daysInMonth = 29;
+            } else {
+                _.daysInMonth = 28;
+            }
+        else
+            _.daysInMonth = 31;
+
+        _.currentMonth = month;
+        _.currentDay = monthDay;
+        _.currentYear = year;
+        _.currentWeekDay = weekDay;
+
+        month+=1;
+        if(month < 10){
+            month = '0' + month;
+        }
+        return year + '-' + month + '-' + monthDay;
+    }
 
     //Методы добавления
     addTask(taskData){
@@ -53,19 +118,62 @@ class List{
             _.tasks[t.date] = [];
         }
         _.tasks[t.date].push(t);
-        _.currentTask = t;
+        _.lastTask = t;
+        localStorage.clear();
         localStorage.setItem(t,JSON.stringify.t);
     }
     addCategory(){}
     addMark(){}
 
+    //Методы отрисовки
+    drawTable(){
+        const _ = this;
+        let calendarBody = document.querySelector('.cal-control');
+        let calendarHtml = '';
+        let daysCnt = _.daysInMonth;
+        for(let i = 1; i <= daysCnt; i++){
+            let buffer = '<button class="cal-control-button">' +
+                '<span class="cal-control-button-day">'+i+'</span>' +
+                '<span class="cal-control-button-do">0</span></button>';
+            calendarHtml += buffer;
+        }
+        calendarBody.innerHTML = calendarHtml;
+    }
+    getFirstDay() {
+        let date = new Date(_.currentYear,_.currentMonth);
+        let firstDay = date.getDay();
+        let marginLeft = 0;
+        if(firstDay === 0){
+            marginLeft = 44 * 6 + 2;
+        } else {
+            marginLeft = 44 * (firstDay - 1) + 2;
+        }
+        let firstDayButton = document.querySelector('.cal-control-button');
+        firstDayButton.style.margin = '3px 2px 0px ' + marginLeft + 'px';
+    }
     //Методы редоктирования
     update(){}
-
+    setMonthName(month=0){
+        const _ = this;
+        let monthCont = document.querySelector('.cal-choose-month');
+        monthCont.textContent = months[_.currentLang][month];
+    }
+    setYearName(year=0){
+        const _ = this;
+        let monthCont = document.querySelector('.cal-choose-year');
+        monthCont.textContent = year;
+    }
     //Методы удаления
     removeCategory(){}
     removeMark(){}
     removeTask(){}
+    // Инициализация программы
+    init(){
+        const _ = this;
+        _.setMonthName(_.currentMonth);
+        _.setYearName(_.currentYear);
+        _.drawTable();
+    }
 }
 
 let list = new List();
