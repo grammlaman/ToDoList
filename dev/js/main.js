044ed81e25c668d01b2c1b887c93bfa4aa7296b2
grammlaman <K1j1u1j1n1b1g1>
2020-01-22 01:23:56 +0600

    21.01.2020

--- dev/js/main.js
+++ dev/js/main.js
@@ -27,7 +27,6 @@ class Task{
         localStorage.setItem('bufferId', _.id);
     }
 }
-
 let months = {
     'ru': [
         'Январь',
@@ -42,27 +41,47 @@ let months = {
         'Октябрь',
         'Ноябрь',
         'Декабрь'
+    ],
+    'en' : [
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+        'July',
+        'August',
+        'September',
+        'october',
+        'November',
+        'December',
     ]
 };
+
+
 
 class List{
     constructor(){
         const _ = this;
         //Свойства
-        /*_.currentMonth = 0;
-        _.currentDay = 0;*/
         _.currentLang = 'ru';
         _.tasks = {};
         _.lastTask = {};
-        //_.getTask();
+        _.calendar = {
+            body : document.querySelector('.cal-control'),
+            monthSelect : document.querySelector('.cal-choose-month'),
+            yearSelect : document.querySelector('.cal-choose-year'),
+            link : document.querySelector('.todayLink')
+        };
         _.currentFullDate = _.getCurrentDate();
+        _.getTask();
         _.init();
     }
     //Методы выборки
     getMonthDays(month){}
     getLabels(day){}
     getCategories(){}
-    getTasks(month){
+    getTask(month){
         const _ = this;
         if(localStorage.getItem('tasks')){
             let tasks = JSON.parse(localStorage.getItem('tasks'));
@@ -75,28 +94,23 @@ class List{
             //_.tasks = JSON.parse(localStorage.getItem('tasks'));
         }
     }
+
     getCurrentDate(dateStr=''){
         const _ = this;
         let date = new Date();
         if(dateStr){
             date = new Date(dateStr);
         }
-        let
-            month = date.getMonth(),
+        let month = date.getMonth(),
             year = date.getFullYear(),
             monthDay = date.getDate(),
             weekDay = date.getDay();
 
-        if( (month == 3) || (month == 5) || (month == 8) || (month == 10) )
-            _.daysInMonth = 30;
+        if((month == 3) || (month == 5) || (month == 8) || (month == 10)){_.daysInMonth = 30;}
         else if(month == 1)
-            if((year%4) == 0){
-                _.daysInMonth = 29;
-            } else {
-                _.daysInMonth = 28;
-            }
-        else
-            _.daysInMonth = 31;
+            if((year%4) == 0){_.daysInMonth = 29;}
+            else {_.daysInMonth = 28;}
+        else{_.daysInMonth = 31;}
 
         _.currentMonth = month;
         _.currentDay = monthDay;
@@ -110,6 +124,86 @@ class List{
         return year + '-' + month + '-' + monthDay;
     }
 
+    getFirstDay() {
+        const _ = this;
+        let date = new Date(_.currentYear + '-' + (_.currentMonth + 1)),
+            firstDay = date.getDay();
+        let marginLeft = 44 * (firstDay - 1) + 2;
+        if(firstDay === 0){marginLeft = 44 * 6 + 2;}
+        let firstDayButton = _.calendar.body.querySelector('.cal-control-button');
+        firstDayButton.style.margin = '3px 2px 0px ' + marginLeft + 'px';
+    }
+    getCurrentDay(){
+        const _ = this;
+        _.calendar.body.children[_.currentDay - 1].classList.add('cal-control-button-active')
+    }
+    clearCurrentDay(){
+        const _ = this;
+        for(let i = 0; i < _.calendar.body.children.length; i++){
+            if(_.calendar.body.children[i].classList.contains('cal-control-button-active')){
+                _.calendar.body.children[i].classList.remove('cal-control-button-active')
+            }
+        }
+    }
+    //Методы отрисовки
+    drawDays(){
+        const _ = this;
+        _.calendar.body.innerHTML = '';
+        let daysCnt = _.daysInMonth;
+        let month = _.currentMonth + 1;
+        if(month < 10){month = '0' + month}
+        for(let i = 1; i <= daysCnt; i++){
+            let dayNumber = i;
+            if(i < 10){dayNumber = '0' + dayNumber}
+            let btn = document.createElement('BUTTON');
+            btn.classList.add('cal-control-button');
+            let span = document.createElement('SPAN');
+            span.classList.add('cal-control-button-day');
+            span.textContent = dayNumber;
+            btn.append(span);
+            span = document.createElement('SPAN');
+            span.classList.add('cal-control-button-do');
+            let str = _.currentYear + '-' + month + '-' +  dayNumber;
+            let doCnt = _.tasks[str];
+            if(doCnt){
+                span.textContent = doCnt.length;
+            } else {
+                span.textContent = '0';
+            }
+            btn.append(span);
+            _.calendar.body.append(btn);
+        }
+        _.getFirstDay();
+    }
+    //Методы редоктирования
+    update(){}
+
+    //Методы создания Select'ов
+    setMonthName(){
+        const _ = this;
+        let monthArr = months[_.currentLang];
+        _.calendar.monthSelect.innerHTML = '';
+        let value = 0;
+        for (let i = 0; i < monthArr.length; i++){
+            value = (i + 1);
+            if(i < 9){
+                value = '0' + value
+            }
+            _.calendar.monthSelect.append(document.createElement('OPTION'));
+            _.calendar.monthSelect.querySelectorAll('option')[i].textContent = monthArr[i];
+            _.calendar.monthSelect.querySelectorAll('option')[i].setAttribute('value',value);
+        }
+    }
+    setYearName(){
+        const _ = this;
+        _.calendar.yearSelect.innerHTML = '';
+        let currentYear = _.currentYear;
+        for (let i = 0; i < 10; i++){
+            _.calendar.yearSelect.append(document.createElement('OPTION'));
+            _.calendar.yearSelect.querySelectorAll('option')[i].textContent = (currentYear + i);
+            _.calendar.yearSelect.querySelectorAll('option')[i].setAttribute('value',(currentYear + i));
+        }
+    }
     //Методы добавления
     addTask(taskData){
         const _ = this;
@@ -125,44 +219,7 @@ class List{
     addCategory(){}
     addMark(){}
 
-    //Методы отрисовки
-    drawTable(){
-        const _ = this;
-        let calendarBody = document.querySelector('.cal-control');
-        let calendarHtml = '';
-        let daysCnt = _.daysInMonth;
-        for(let i = 1; i <= daysCnt; i++){
-            let buffer = '<button class="cal-control-button">' +
-                '<span class="cal-control-button-day">'+i+'</span>' +
-                '<span class="cal-control-button-do">0</span></button>';
-            calendarHtml += buffer;
-        }
-        calendarBody.innerHTML = calendarHtml;
-    }
-    getFirstDay() {
-        let date = new Date(_.currentYear,_.currentMonth);
-        let firstDay = date.getDay();
-        let marginLeft = 0;
-        if(firstDay === 0){
-            marginLeft = 44 * 6 + 2;
-        } else {
-            marginLeft = 44 * (firstDay - 1) + 2;
-        }
-        let firstDayButton = document.querySelector('.cal-control-button');
-        firstDayButton.style.margin = '3px 2px 0px ' + marginLeft + 'px';
-    }
-    //Методы редоктирования
-    update(){}
-    setMonthName(month=0){
-        const _ = this;
-        let monthCont = document.querySelector('.cal-choose-month');
-        monthCont.textContent = months[_.currentLang][month];
-    }
-    setYearName(year=0){
-        const _ = this;
-        let monthCont = document.querySelector('.cal-choose-year');
-        monthCont.textContent = year;
-    }
+
     //Методы удаления
     removeCategory(){}
     removeMark(){}
@@ -170,9 +227,10 @@ class List{
     // Инициализация программы
     init(){
         const _ = this;
-        _.setMonthName(_.currentMonth);
-        _.setYearName(_.currentYear);
-        _.drawTable();
+        _.setMonthName();
+        _.setYearName();
+        _.drawDays();
+        _.getCurrentDay();
     }
 }
 
@@ -181,14 +239,14 @@ list.addTask({
     title: 'Покушать',
     description: 'Люблю кушать',
     catID: 2,
-    date: '2019-12-31',
+    date: '2020-01-31',
     marks: [1,3,4]
 });
 list.addTask({
     title: 'Пить',
     description: 'Люблю кушать',
     catID: 2,
-    date: '2019-12-31',
+    date: '2020-01-31',
     marks: [1,3,4]
 });
 list.addTask({
@@ -200,114 +258,39 @@ list.addTask({
 });
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-let date = new Date();
-let calendar = {
-    item : document.getElementById('calendar'),
-    year : document.querySelector('.cal-choose-year'),
-    month : document.querySelector('.cal-choose-month'),
-    day : document.querySelector('.cal-control')
-};
-// Активный месяц и год по умолчанию
-function calendarNewSelects() {
-    let thisMonth = date.getMonth();
-    let thisYear = date.getFullYear();
-    calendar.month.options.selectedIndex = thisMonth;
-    calendar.year.value = thisYear;
-}
-calendarNewSelects();
-//Формирование кнопок дней в зависимости от месяца
-function CreatCalendarButtons() {
-    let calHTML = '';
-    let month = calendar.month.options.selectedIndex;
-    let year = calendar.year.value;
-    let daysCnt = 0;
-    if((month == 3)||(month == 5)||(month == 8)||(month == 10)){daysCnt = 30;}
-    else if(month == 1){
-        if((year % 4) == 0){
-            daysCnt = 29
-        } else {
-            daysCnt = 28
-        }
+list.calendar.monthSelect.addEventListener('change',function (el) {
+    let dateStr = list.calendar.yearSelect.value + '-' + list.calendar.monthSelect.value;
+    list.getCurrentDate(dateStr);
+    list.drawDays();
+});
+list.calendar.yearSelect.addEventListener('change',function (el) {
+    let dateStr = list.calendar.yearSelect.value + '-' + list.calendar.monthSelect.value;
+    list.getCurrentDate(dateStr);
+    list.drawDays();
+});
+list.calendar.body.addEventListener('click',function (el) {
+    let clickTarget = el.target;
+    if(clickTarget.tagName == 'DIV') return;
+    if(clickTarget.tagName == 'SPAN'){
+        clickTarget = clickTarget.parentElement;
     }
-    else {daysCnt = 31}
-    for(let i = 1; i <= daysCnt; i++){
-        let a = '<button class="cal-control-button">' +
-            '<span class="cal-control-button-day">'+i+'</span>' +
-            '<span class="cal-control-button-do">0</span></button>';
-        calHTML += a;
+    list.clearCurrentDay();
+    clickTarget.classList.add('cal-control-button-active');
+    let calendarDay = clickTarget.querySelector('.cal-control-button-day').textContent;
+    if(calendarDay < 10){
+        calendarDay = '0' + calendarDay;
     }
-    calendar.day.innerHTML = calHTML;
-    calendar.day.querySelector('button').classList.add('cal-control-button-first');
-}
-CreatCalendarButtons();
-
-// Активный день по умолчанию
-function calendarPickActive(){
-    let calendarDays = calendar.day.querySelectorAll('.cal-control-button');
-    let today = date.getDate();
-    calendarDays[today-1].classList.add('cal-control-button-active');
-}
-calendarPickActive();
-
-// Выбор дня недели на первое число
-function getFirstDay() {
-    let date = new Date(calendar.year.value,calendar.month.options.selectedIndex,1);
-    let firstDay = date.getDay();
-    let marginLeft = 0;
-    if(firstDay === 0){
-        marginLeft = 44 * 6 + 2;
+    list.calendar.link.textContent = calendarDay +'.' + list.calendar.monthSelect.value +
+        '.' + list.calendar.yearSelect.value;
+    let doDate = list.calendar.yearSelect.value + '-' + list.calendar.monthSelect.value +
+        '-' + calendarDay;
+    let tasks = [];
+    tasks = list.tasks[doDate];
+    if(tasks){
+        console.log(tasks)
     } else {
-        marginLeft = 44 * (firstDay - 1) + 2;
+        console.log('Запланированных дел нет :(')
     }
-    let firstDayButton = document.querySelector('.cal-control-button-first');
-    firstDayButton.style.margin = '3px 2px 0px ' + marginLeft + 'px';
-}
-getFirstDay();
-calendar.month.addEventListener('change',function (el) {
-    CreatCalendarButtons();
-    getFirstDay();
-});
-calendar.year.addEventListener('change',function (el) {
-    CreatCalendarButtons();
-    getFirstDay();
+
 });
 
-// Переключение дня по клику
-function dayClick(el) {
-    let calendarDays = calendar.day.querySelectorAll('.cal-control-button');
-    calendarDays.forEach(function (el) {
-        if(el.classList.contains('cal-control-button-active')) el.classList.remove('cal-control-button-active');
-    });
-    el.classList.add('cal-control-button-active');
-}
-calendar.day.addEventListener('click',function (el) {
-    let element = el.target;
-    if(element != calendar.day){
-        if(!(element.tagName == "BUTTON") ){
-            while(element.tagName != 'BUTTON'){
-                element = element.parentNode;
-            }
-        }
-        dayClick(element);
-    }
-    let doDate = document.querySelector('.todayLink');
-    let doDateDay = element.querySelector('.cal-control-button-day').textContent;
-    if(doDateDay < 10) doDateDay = '0' + doDateDay;
-    let doDateMonth = calendar.month.options.selectedIndex + 1;
-    if(doDateMonth < 10) doDateMonth = '0' + doDateMonth;
-    let doDateYear = calendar.year.value;
-    doDate.textContent = doDateDay + '.' + doDateMonth + '.' + doDateYear;
-});
